236acdc218efb6d8f73a4b243ed8118a922869be
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-27 20:00:00 +0200

    feat: add benchmark for barnes hut gravitational forces

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,14 +1,30 @@
+import { Body, BodyType } from '../src/Body';
+import { G, SETTINGS } from '../src/Constants';
+import { applyBarnesHutGravitationalForces, applyGravitationalForces } from '../src/Gravity';
+import { randomNumber } from '../src/Math';
+
 declare const process: {
     on(event: 'exit', listener: () => void): void;
 };
 
+const numBodies = 1_000;
+const bodies: Body[] = [];
+
+for (let i = 0; i < numBodies; i++) {
+    const x = randomNumber(1e-8, 1e8);
+    const y = randomNumber(1e-8, 1e8);
+    const radius = randomNumber(10_000, 100_000);
+    const mass = randomNumber(1e16, 1e24);
+    const b = new Body(x, y, radius, mass, BodyType.PLANET);
+    bodies.push(b);
+}
 
 export function runOriginal() {
-    //
+    applyGravitationalForces(bodies, G);
 }
 
 export function runModified() {
-    //
+    applyBarnesHutGravitationalForces(bodies, G);
 }
 
 process.on('exit', () => {

16aef1c92e02cdee080f413679b8b21bf5bc0c95
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-05-04 20:00:00 +0200

    feat: add time benchmarking

--- wasm/main.c
+++ wasm/main.c
@@ -2,6 +2,7 @@
 #include "engine.h"
 #include "constants.h"
 #include "body.h"
+#include <time.h>
 
 int main(void)
 {
@@ -12,6 +13,23 @@ int main(void)
     printf("Body count: %d\n", getBodyCount());
     removeBody(0);
     printf("Body count: %d\n", getBodyCount());
-    printf("DT: %f", simulation_dt());
+    printf("DT: %f\n", simulation_dt());
+    addNewBody(0, 0, 10, 10, 1, 1, 1, -1);
+    addNewBody(100, 0, 10, 10, 1, 1, 1, -1);
+    addNewBody(0, 100, 10, 10, 1, 1, 1, -1);
+    addNewBody(100, 100, 10, 10, 1, 1, 1, -1);
+    printf("Body count: %d\n", getBodyCount());
+
+    // Benchmarking
+    clock_t start = clock();
+
+    clock_t end = clock();
+
+    update(simulation_dt());
+
+    double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Time: %f seconds\n", elapsed);
+
     return 0;
 }

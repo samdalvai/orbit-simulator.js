5ca6d0e1cdbed784ef31914ddcf816e022407e74
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-05-04 20:00:00 +0200

    feat: add basic packed bodies arrays

--- wasm/body.c
+++ wasm/body.c
@@ -1,1 +1,33 @@
+#include <stdint.h>
+
 #include "body.h"
+#include "constants.h"
+
+enum BodyType
+{
+    STAR,
+    PLANET,
+    MOON,
+    ASTEROID
+};
+
+extern int parents[MAX_BODIES];
+extern uint32_t bodyTypes[MAX_BODIES];
+
+extern double radiuses[MAX_BODIES];
+
+// Linear motion
+extern double posX[MAX_BODIES];
+extern double posY[MAX_BODIES];
+extern double velX[MAX_BODIES];
+extern double velY[MAX_BODIES];
+extern double accX[MAX_BODIES];
+extern double accY[MAX_BODIES];
+
+// Forces
+extern double sumForcesX[MAX_BODIES];
+extern double sumForcesY[MAX_BODIES];
+
+// Mass
+extern double masses[MAX_BODIES];
+extern double invMasses[MAX_BODIES];
---
+++ wasm/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_BODIES 10000
+#define G 6.6743e-20
+
+#endif

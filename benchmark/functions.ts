3c8ee8760add23502d36119dcebcda06090beba2
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-05-13 20:00:00 +0200

    feat: reset benchmark

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,16 +1,15 @@
-import { SETTINGS } from '../src/Constants';
-import { Engine } from '../src/Engine';
-import { randomNumber } from '../src/Math';
 import { BodyType, NO_PARENT, addNewBody, getBodyCount } from '../src/Body';
-import { mass, positionX, positionY } from '../src/Body';
-import { Engine as EnginePacked } from '../src/Engine';
+import { G, SETTINGS } from '../src/Constants';
+import { Engine } from '../src/Engine';
+import { applyBarnesHutGravitationalForces, applyGravitationalForces } from '../src/Gravity';
+import { randomNumber } from '../src/Math';
+import { Vec2 } from '../src/Vec2';
 
 declare const process: {
     on(event: 'exit', listener: () => void): void;
 };
 
 const engine = new Engine();
-const enginePacked = new EnginePacked();
 
 const numBodies = 10_000;
 
@@ -19,44 +18,25 @@ for (let i = 0; i < numBodies; i++) {
     const y = randomNumber(1e-8, 1e8);
     const radius = randomNumber(10_000, 100_000);
     const mass = randomNumber(1e16, 1e24);
-    const b = new Body(x, y, radius, mass, BodyType.PLANET);
-
-    engine.addBody(b);
-    addNewBody(
-        b.position.x,
-        b.position.y,
-        b.radius,
-        b.mass,
-        b.bodyType,
-        b.velocity,
-        b.parent ? b.parent.id : NO_PARENT,
-    );
+    addNewBody(x, y, radius, mass, BodyType.PLANET, new Vec2(), NO_PARENT);
 }
 
-// const WARM_UP_ITERATIONS = 1_000;
-const WARM_UP_ITERATIONS = 0;
+const WARM_UP_ITERATIONS = 1000;
 
 for (let i = 0; i < WARM_UP_ITERATIONS; i++) {
     engine.update(SETTINGS.dt);
-    enginePacked.update(SETTINGS.dt);
 }
 
 export function runOriginal() {
-    // applyGravitationalForces(bodies, G);
-    // applyBarnesHutGravitationalForces(bodies, G);
-    engine.update(SETTINGS.dt);
+    applyGravitationalForces(G);
+    // engine.update(SETTINGS.dt);
 }
 
 export function runModified() {
-    // applyBarnesHutGravitationalForces(bodies, G);
-    // applyPackedBarnesHutGravitationalForces(bodies, G);
-    enginePacked.update(SETTINGS.dt);
+    applyBarnesHutGravitationalForces(G);
+    // engine.update(SETTINGS.dt);
 }
 
 process.on('exit', () => {
-    console.log('Body count engine: ', engine.getBodiesCount());
-    console.log('Body count engine packed: ', getBodyCount());
-
-    console.log('First body engine: ', engine.getBodies()[0]);
-    console.log(`First body engine packed: posX ${positionX[0]} posY ${positionY[0]} mass ${mass[0]}`);
+    console.log('Body count engine: ', getBodyCount());
 });

49f4f3a5e1aa20b0596380117e9b7c4a904ad8c8
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-05-17 20:00:00 +0200

    feat: add 3d camera

---
+++ src/view/Camera3D.ts
@@ -0,0 +1,43 @@
+export class Camera3D {
+    x = 0;
+    y = 0;
+    z = -1000;
+
+    focalLength = 800;
+
+    screenWidth: number;
+    screenHeight: number;
+
+    near = 1;
+
+    constructor(screenWidth: number, screenHeight: number) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+    }
+
+    project(x: number, y: number, z: number): ProjectedPoint | null {
+        const cameraX = x - this.x;
+        const cameraY = y - this.y;
+        const cameraZ = z - this.z;
+
+        if (cameraZ <= this.near) {
+            return null;
+        }
+
+        const scale = this.focalLength / cameraZ;
+
+        return {
+            x: this.screenWidth * 0.5 + cameraX * scale,
+            y: this.screenHeight * 0.5 - cameraY * scale,
+            scale,
+            depth: cameraZ,
+        };
+    }
+}
+
+export type ProjectedPoint = {
+    x: number;
+    y: number;
+    scale: number;
+    depth: number;
+};

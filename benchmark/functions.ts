0d087139fce13e356ca49ade5c09cd96c7d17cf4
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-05-22 20:00:00 +0200

    feat: update benchmark code

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,24 +1,27 @@
-import { BodyType, NO_PARENT, addNewBody, getBodyCount } from '../src/sim/Body';
-import { Engine } from '../src/sim/Engine';
-import { applyBarnesHutGravitationalForces, applyGravitationalForces } from '../src/sim/Gravity';
 import { G, SETTINGS } from '../src/shared/Constants';
 import { randomNumber } from '../src/shared/Math';
 import { Vec3 } from '../src/shared/Vec3';
+import { BodyType, NO_PARENT, addNewBody, getBodyCount } from '../src/sim/Body';
+import { Engine } from '../src/sim/Engine';
+import { applyBarnesHutGravitationalForces, applyGravitationalForces } from '../src/sim/Gravity';
+import { BodyRenderStyle } from '../src/view/BodyRenderStyle';
 
 declare const process: {
     on(event: 'exit', listener: () => void): void;
 };
 
-const engine = new Engine();
+const bodyRenderStyles = new Map<number, BodyRenderStyle>();
+const engine = new Engine(bodyRenderStyles);
 
 const numBodies = 10_000;
 
 for (let i = 0; i < numBodies; i++) {
     const x = randomNumber(1e-8, 1e8);
     const y = randomNumber(1e-8, 1e8);
+    const z = randomNumber(1e-8, 1e8);
     const radius = randomNumber(10_000, 100_000);
     const mass = randomNumber(1e16, 1e24);
-    addNewBody(x, y, radius, mass, BodyType.PLANET, new Vec3(), NO_PARENT);
+    addNewBody(x, y, z, radius, mass, BodyType.PLANET, new Vec3(), NO_PARENT);
 }
 
 const WARM_UP_ITERATIONS = 1000;
@@ -28,12 +31,12 @@ for (let i = 0; i < WARM_UP_ITERATIONS; i++) {
 }
 
 export function runOriginal() {
-    applyGravitationalForces(G);
+    applyGravitationalForces();
     // engine.update(SETTINGS.dt);
 }
 
 export function runModified() {
-    applyBarnesHutGravitationalForces(G);
+    applyBarnesHutGravitationalForces([]);
     // engine.update(SETTINGS.dt);
 }
 
